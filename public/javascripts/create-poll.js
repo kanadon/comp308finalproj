ca852a4fb6d001ebc789cd5528026e9d083557fd
Denis <ddoubovtsev@gmail.com>
2017-04-17 23:54:35 -0400

    - templates for question creation

--- public/javascripts/create-poll.js
+++ public/javascripts/create-poll.js
@@ -3,35 +3,45 @@
  */
 
 document.addEventListener("DOMContentLoaded", function () {
-  var tfInputs = document.querySelectorAll(".tf-input");
-  var mcInputs = document.querySelectorAll(".mc-input");
-  var typeSelector = document.querySelector(".type-selector");
-
-  // when user changes poll type, show/hide relevant/irrelevant fields for the selected type
-
-  typeSelector.addEventListener("change", function () {
-    var type = this[this.selectedIndex].value;
-    if(type == 'tf'){
-      mcInputs.forEach(function (input) {
-        input.setAttribute("hidden", "");
-        input.setAttribute("disabled", "");
-      });
-
-      tfInputs.forEach(function (input) {
-        input.removeAttribute("hidden", "");
-        input.removeAttribute("disabled", "");
-      });
-    }
-    else if(type == 'mc'){
-      tfInputs.forEach(function (input) {
-        input.setAttribute("hidden", "");
-        input.setAttribute("disabled", "");
-      });
-
-      mcInputs.forEach(function (input) {
-        input.removeAttribute("hidden", "");
-        input.removeAttribute("disabled", "");
-      });
-    }
+  QuestionAdder.Init({
+    questionsContainer: document.querySelector('#questions'),
+    typeSelector: document.querySelector('#question-type'),
+    numberOfOptionsLabel: document.querySelector('#number-of-options'),
+    numberOfOptionsInput: document.querySelector('#number-of-options input'),
+    addBtn: document.querySelector('#add-question'),
+    templates: document.querySelectorAll('.template')
   });
 });
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   var tfInputs = document.querySelectorAll(".tf-input");
+//   var mcInputs = document.querySelectorAll(".mc-input");
+//   var typeSelector = document.querySelector(".question-type-selector");
+//
+//   typeSelector.addEventListener("change", function () {
+//     var type = this[this.selectedIndex].value;
+//     if(type == 'tf'){
+//       mcInputs.forEach(function (input) {
+//         input.setAttribute("hidden", "");
+//         input.setAttribute("disabled", "");
+//       });
+//
+//       tfInputs.forEach(function (input) {
+//         input.removeAttribute("hidden", "");
+//         input.removeAttribute("disabled", "");
+//       });
+//     }
+//     else if(type == 'mc'){
+//       tfInputs.forEach(function (input) {
+//         input.setAttribute("hidden", "");
+//         input.setAttribute("disabled", "");
+//       });
+//
+//       mcInputs.forEach(function (input) {
+//         input.removeAttribute("hidden", "");
+//         input.removeAttribute("disabled", "");
+//       });
+//     }
+//   });
+// });
---
+++ public/javascripts/questionAdder.js
@@ -0,0 +1,69 @@
+/**
+ * Created by DENIS on 17/04/2017.
+ */
+
+var QuestionAdder = (function () {
+  var
+    questionsContainer,
+    typeSelector,
+    numberOfOptionsLabel,
+    numberOfOptionsInput,
+    addBtn,
+    templates = {},
+    currentOrder = 1;
+
+  function init(options) {
+    Handlebars.registerHelper('iterate', function (n, options) {
+      var aggregate = '';
+
+      for (var i = 0; i < n; i++) {
+        aggregate += options.fn({
+          index: i + 1
+        });
+      }
+
+      return aggregate;
+    });
+
+    questionsContainer = options.questionsContainer;
+    typeSelector = options.typeSelector;
+    numberOfOptionsLabel = options.numberOfOptionsLabel;
+    numberOfOptionsInput = options.numberOfOptionsInput;
+    addBtn = options.addBtn;
+    // templates = options.templates;
+
+    options.templates.forEach(function (template) {
+      var type = template.getAttribute('data-type');
+      templates[type] = Handlebars.compile(template.innerHTML);
+    });
+
+    typeSelector.addEventListener("change", function () {
+      var type = this[this.selectedIndex].value;
+
+      if (type == 'mc')
+        numberOfOptionsLabel.classList.remove('hidden');
+      else
+        numberOfOptionsLabel.classList.add('hidden');
+    });
+
+    addBtn.addEventListener('click', function () {
+      addQuestion(typeSelector[typeSelector.selectedIndex].value, numberOfOptionsInput.value);
+    });
+  }
+
+  function addQuestion(type, numberOfOptions) {
+    var
+      template = templates[type],
+      model = {
+        order: currentOrder++,
+        numberOfOptions: numberOfOptions
+      },
+      html = template(model);
+
+    questionsContainer.innerHTML += html;
+  }
+
+  return {
+    Init: init
+  };
+})();
